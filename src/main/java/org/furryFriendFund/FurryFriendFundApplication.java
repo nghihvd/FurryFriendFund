1c11f0faa98335104d89ba9a67c8fd21a1afb77f
nghihvd <hoangvodonghi@gmail.com>
2024-09-12 23:35:18 +0700

    InitProject

--- src/main/java/org/furryFriendFund/FurryFriendFundApplication.java
+++ src/main/java/org/furryFriendFund/FurryFriendFundApplication.java
@@ -1,4 +1,4 @@
-package org.FurryFund;
+package org.furryFriendFund;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/org/furryFriendFund/FurryFriendFundApplicationTests.java
+++ src/test/java/org/furryFriendFund/FurryFriendFundApplicationTests.java
@@ -1,4 +1,4 @@
-package org.FurryFund;
+package org.furryFriendFund;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
