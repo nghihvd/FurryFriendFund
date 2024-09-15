403f25aa28f699f1f75c5db8b94e3860f197390d
Đông Nghi <82652596+nghihvd@users.noreply.github.com>
2024-09-15 15:56:35 +0700

    Update UserRepository.java
    xóa bớt dòng import repository chỉ để lại JpaRepository

--- src/main/java/org/furryFriendFund/user/UserRepository.java
+++ src/main/java/org/furryFriendFund/user/UserRepository.java
@@ -1,7 +1,6 @@
 package org.furryFriendFund.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 public interface UserRepository extends JpaRepository<UsersDTO, String> {
 
