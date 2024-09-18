96b32c863ee819b6cd79cce1849db1004bec7295
trgphucloc <loc0569157095@gmail.com>
2024-09-18 19:43:07 +0700

    updateCode

--- src/main/java/org/example/furryfriendfund/accounts/IAccountsService.java
+++ src/main/java/org/example/furryfriendfund/accounts/IAccountsService.java
@@ -2,7 +2,6 @@ package org.example.furryfriendfund.accounts;
 
 public interface IAccountsService {
     Accounts saveAccountsInfo(Accounts user);
-    public void update(Accounts user);
     public boolean ckLogin(String userID,String password);
     public void logout();
     public Accounts getUserById(String userID);
