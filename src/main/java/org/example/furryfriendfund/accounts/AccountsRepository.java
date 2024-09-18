084ac988f530691c084a1d600cd99bab547c0b8d
Đông Nghi <82652596+nghihvd@users.noreply.github.com>
2024-09-18 19:30:31 +0700

    Update AccountsRepository.java

--- src/main/java/org/example/furryfriendfund/accounts/AccountsRepository.java
+++ src/main/java/org/example/furryfriendfund/accounts/AccountsRepository.java
@@ -1,7 +1,9 @@
 package org.example.furryfriendfund.accounts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AccountsRepository extends JpaRepository<Accounts,String> {
 //JpaRepo sẽ tự động tạo ra các câu lệnh SQL ph hợp mà không cần viết tay
     
