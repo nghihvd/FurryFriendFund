dd99140c5c4753eaa3114fc7f7959215e45ea0ac
Đông Nghi <82652596+nghihvd@users.noreply.github.com>
2024-09-19 16:39:35 +0700

    update

--- src/main/java/org/example/furryfriendfund/accounts/Accounts.java
+++ src/main/java/org/example/furryfriendfund/accounts/Accounts.java
@@ -1,9 +1,6 @@
 package org.example.furryfriendfund.accounts;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
