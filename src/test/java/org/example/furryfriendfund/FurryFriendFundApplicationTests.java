034f6d63fd75f7439ae6230cf5afd6c019b0723f
Đông Nghi <82652596+nghihvd@users.noreply.github.com>
2024-09-20 19:23:19 +0700

    update

--- src/test/java/org/example/furryfriendfund/FurryFriendFundApplicationTests.java
+++ src/test/java/org/example/furryfriendfund/FurryFriendFundApplicationTests.java
@@ -1,5 +1,6 @@
-package org.example.furryfriendfund;
+package org.example.furryFriendFund;
 
+import org.example.furryfriendfund.FurryFriendFundApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
