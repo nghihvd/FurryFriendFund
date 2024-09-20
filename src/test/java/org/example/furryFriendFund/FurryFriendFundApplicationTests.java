0f71c148c40d81f00e6c70a41e2250786618a57b
Đông Nghi <82652596+nghihvd@users.noreply.github.com>
2024-09-20 19:08:23 +0700

    update

--- src/test/java/org/example/furryFriendFund/FurryFriendFundApplicationTests.java
+++ src/test/java/org/example/furryFriendFund/FurryFriendFundApplicationTests.java
@@ -1,9 +1,10 @@
 package org.example.furryFriendFund;
 
+import org.example.furryfriendfund.FurryFriendFundApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = FurryFriendFundApplication.class)
 class FurryFriendFundApplicationTests {
 
 	@Test
