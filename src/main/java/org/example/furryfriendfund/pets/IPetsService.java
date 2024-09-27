5f852d0d016e339f540cf4c01113dc0e8dce8e10
Gia Bảo Huỳnh <baohgse182507@fpt.edu.vn>
2024-09-27 10:18:58 +0700

    update Pets functions

--- src/main/java/org/example/furryfriendfund/pets/IPetsService.java
+++ src/main/java/org/example/furryfriendfund/pets/IPetsService.java
@@ -6,6 +6,5 @@ public interface IPetsService {
     Pets addPet(Pets pet);
     List<Pets> searchPetsByNameAndBreed(String name,float age,String sex, int categoryID,String breed);
     List<Pets> showList();
-    List<Pets> showPetsById(String petId);
     Pets findPetById(String petId);
 }
