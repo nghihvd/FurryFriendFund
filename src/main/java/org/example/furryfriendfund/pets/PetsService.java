59010aa6d3281a4c2fdab6122265029d08138b3c
Gia Bảo Huỳnh <baohgse182507@fpt.edu.vn>
2024-10-01 10:54:00 +0700

    update PetServices-Repo

--- src/main/java/org/example/furryfriendfund/pets/PetsService.java
+++ src/main/java/org/example/furryfriendfund/pets/PetsService.java
@@ -43,8 +43,8 @@ public class PetsService implements IPetsService {
 
     @Override
     public List<Pets> searchPetsByNameAndBreedAdmin(String name, float age, String sex, int categoryID, String breed) {
-        List<Pets> petsInName = petsRepository.findByNameContainingIgnoreCase(name);
-        List<Pets> petsInBreed = petsRepository.findByBreedContainingIgnoreCase(breed);
+        List<Pets> petsInName = petsRepository.findByNameIgnoreCase(name);
+        List<Pets> petsInBreed = petsRepository.findByBreedIgnoreCase(breed);
 
         Set<Pets> uniquePets = new HashSet<>(petsInName);
         uniquePets.addAll(petsInBreed);
@@ -55,8 +55,8 @@ public class PetsService implements IPetsService {
 
     @Override
     public List<Pets> searchPetsByNameAndBreed(String name, float age, String sex, int categoryID, String breed) {
-        List<Pets> petsInName = petsRepository.findByNameContainingIgnoreCase(name);
-        List<Pets> petsInBreed = petsRepository.findByBreedContainingIgnoreCase(breed);
+        List<Pets> petsInName = petsRepository.findByNameIgnoreCase(name);
+        List<Pets> petsInBreed = petsRepository.findByBreedIgnoreCase(breed);
 
         Set<Pets> uniquePets = new HashSet<>(petsInName);
         uniquePets.addAll(petsInBreed);
