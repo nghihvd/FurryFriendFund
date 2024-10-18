b1247a8d92865711e20f02ca2f96d9113c95f859
Đông Nghi <82652596+nghihvd@users.noreply.github.com>
2024-10-18 14:32:32 +0700

    update swagger

--- src/main/java/org/example/furryfriendfund/config/SwaggerConfig.java
+++ src/main/java/org/example/furryfriendfund/config/SwaggerConfig.java
@@ -1,4 +1,5 @@
 package org.example.furryfriendfund.config;
+import springfox.documentation.service.Contact;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.servers.Server;
 
@@ -10,6 +11,12 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 import java.util.List;
 
@@ -34,6 +41,25 @@ public class SwaggerConfig {
                                         .bearerFormat("JWT")
                         )
                 );
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("org.example.furryfriendfund.controllers")) // Package chứa các API
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("API Documentation")
+                .description("API documentation for Spring Boot application")
+                .version("1.0")
+                .contact(new Contact("Furry Fund","https://fundfe.vercel.app","nghihvdse182563@fpt.edu.vn"))
+                .build();
     }
 
 }
