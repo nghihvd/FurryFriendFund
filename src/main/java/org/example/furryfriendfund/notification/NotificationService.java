e526cc9286fdf9729a7c456dc7f97c24424003f3
Gia Bảo Huỳnh <baohgse182507@fpt.edu.vn>
2024-10-24 18:58:20 +0700

    update EventNotifications

--- src/main/java/org/example/furryfriendfund/notification/NotificationService.java
+++ src/main/java/org/example/furryfriendfund/notification/NotificationService.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
