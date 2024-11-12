4c0e05a7951c81415debacc10e66c62fe6a8f386
Đông Nghi <82652596+nghihvd@users.noreply.github.com>
2024-11-12 16:27:13 +0700

    local swagger

--- src/main/java/org/example/furryfriendfund/config/WebSecurityConfig.java
+++ src/main/java/org/example/furryfriendfund/config/WebSecurityConfig.java
@@ -74,6 +74,7 @@ public class WebSecurityConfig   {
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
+
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
