7879058c691b9b8e8f6ebd3669e18f4009d53bce
Đông Nghi <82652596+nghihvd@users.noreply.github.com>
2024-11-16 17:10:06 +0700

    show return pet

--- src/main/java/org/example/furryfriendfund/OTP/EmailService.java
+++ src/main/java/org/example/furryfriendfund/OTP/EmailService.java
@@ -8,7 +8,6 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 
 @Service
 public class EmailService {
