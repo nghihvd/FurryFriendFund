1d552580599fcb3192dbfcd49444f6940681977b
Đông Nghi <82652596+nghihvd@users.noreply.github.com>
2024-11-18 18:59:46 +0700

    img

--- src/main/java/org/example/furryfriendfund/config/WebSecurityConfig.java
+++ src/main/java/org/example/furryfriendfund/config/WebSecurityConfig.java
@@ -77,7 +77,7 @@ public class WebSecurityConfig   {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("https://fundfe.vercel.app","http://localhost:3000","https://trial-fe.vercel.app"));
+        configuration.setAllowedOrigins(Arrays.asList("https://fundfe.vercel.app","http://localhost:3000","https://trial-fe.vercel.app","https://yourapp.onrender.com"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
