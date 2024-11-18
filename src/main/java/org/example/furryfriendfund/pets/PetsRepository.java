21f91f55d4ff712c816d4202cf4556d2ed74ecdd
Đông Nghi <82652596+nghihvd@users.noreply.github.com>
2024-11-18 20:27:55 +0700

    update dash board

--- src/main/java/org/example/furryfriendfund/pets/PetsRepository.java
+++ src/main/java/org/example/furryfriendfund/pets/PetsRepository.java
@@ -10,17 +10,10 @@ public interface PetsRepository extends JpaRepository<Pets, String> {
     List<Pets> findByNameIgnoreCaseAndTrimmed(@Param("name") String name);
     List<Pets> getPetsByAccountID(String accountID);
 
-    @Query("SELECT COUNT(p) FROM Pets p WHERE p.accountID is not null and p.status = 'Unavailable'")
-    int countPetAdopted();
 
-    @Query("SELECT COUNT(p) FROM Pets p WHERE p.accountID is null and p.status = 'Available'")
-    int countPetAvailable();
 
     @Query("SELECT COUNT(p) FROM Pets p WHERE p.accountID is not null and p.status = :status ")
     int countPetStatus(@Param("status") String status);
-
-    @Query("SELECT COUNT(p) FROM Pets p WHERE p.accountID is null and p.status = 'Unavailable'")
-    int countPetUnavailable();
 
     @Query("select a from Pets a where a.petID = :id")
     Pets getPetByPetID(@Param("id") String id);
